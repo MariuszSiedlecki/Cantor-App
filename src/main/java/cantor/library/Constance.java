e621777ad622f4dc34be6c8f2a2450515491c0a1
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-10-15 02:03:26 +0200

    menu button

--- src/main/java/cantor/library/Constance.java
+++ src/main/java/cantor/library/Constance.java
@@ -2,4 +2,9 @@ package cantor.library;
 
 public class Constance {
     public static final String APP_VERSION = "Application Cantor v.1.0.0";
+    public static final String API_KEY = "&apiKey=cd8b88536350a5db7a5e";
+    public static final String INFO_TYPE = "&compact=ultra";
+    public static final String CURRENCY_SERVICE = "https://free.currconv.com/api/v7/convert?q=";
+    public static final String NODE = "_";
+    public static final String MARGIN_VALUE = "0.8";
 }
