b6fb38f40bd8da87e324f2a48d535ab7b57c0b2b
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-10-17 13:17:23 +0200

    Librarys

--- src/main/java/cantor/App.java
+++ src/main/java/cantor/App.java
@@ -1,10 +1,13 @@
 package cantor;
 
+import cantor.library.Constance;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import static cantor.library.Constance.*;
 
 public class App extends Application {
 
@@ -19,8 +22,14 @@ public class App extends Application {
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/main-screen.fxml"));
+        StackPane stackPane = loader.load();
+        Scene scene = new Scene(stackPane, 400, 400);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle(APP_VERSION);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
