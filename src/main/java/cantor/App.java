e8093f32ac0dd885ec3b65ac3354d6b0f223da78
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-10-26 23:23:09 +0200

    exchange office

--- src/main/java/cantor/App.java
+++ src/main/java/cantor/App.java
@@ -11,15 +11,8 @@ import static cantor.library.Constance.*;
 
 public class App extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/main-screen.fxml"));
-        StackPane stackPane = loader.load();
-        Scene scene = new Scene(stackPane,400,400);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Application Cantor v.1.0.0");
-        primaryStage.setResizable(false);
-        primaryStage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
